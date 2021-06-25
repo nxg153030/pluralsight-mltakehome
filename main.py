170a9c34a1ae11c0b38308a363f6aa007a12da03
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2021-06-25 07:20:44 -0600

    Inefficient working implementation

--- main.py
+++ main.py
@@ -1,33 +1,64 @@
 import flask
+import numpy as np
+import csv
 
 app = flask.Flask(__name__)
 
 
-def compute_similarity_score(user_handle_1, user_handle_2):
+def load_feature_matrix():
     pass
 
 
+def compute_similarity_score(user1_feature_vec, user2_feature_vec):
+    """
+    1. Fetch all the courses the user1 and user2 have taken. Count no. of matching courses taken
+        1.a: What about course tags?
+    2. Fetch their interests. Count no. of matching interests
+    3. Fetch all author handles of the course. (Add more weight to this?)
+    4. How to use user assessment scores?
+    5. How to use viewing time?
+    The time of day when users watch a course. Users who are consistent
+    I don't have a class to work with.
+      |__c1___|__c2___|__c3___|
+    u1|_______|_______|_______|
+    u2|_______|_______|_______|
+    Logic:
+    1. Fetch feature_matrix
+    2. Compute cosine distance
+    3.
+    """
+    cosine_similarity = np.dot(user1_feature_vec, user2_feature_vec)/(np.linalg.norm(user1_feature_vec) * np.linalg.norm(user2_feature_vec))
+    return round(cosine_similarity, 4)
+
+
 def get_top_n_similar_users(user_handle, n=5):
-    # fetch the user record
-    # compute similarity score between passed user and every other user in the db
-    # sort the results by most similar to least similar
-    # return user handles of top 5 most similar users
-    top_n_user_handles = []
-    user_handles = get_all_user_handles()
-    user_similarity_score_mappings = {}
+    try:
+        feature_matrix = np.loadtxt('data/user_feature_matrix.csv', skiprows=1, delimiter=',')  # expensive operation
+    except Exception as e:
+        print('ERROR: Unable to load feature matrix')
+        raise Exception()
+    user_feature_vec = feature_matrix[user_handle-1][1:]  # ignore first column which contains user handles
+    user_feature_vec = user_feature_vec
+    user_handles = feature_matrix[:, 0]
     top_n_user_handles = []
     for user_handle_2 in user_handles:
-        similarity_score = compute_similarity_score(user_handle, user_handle_2)
-        user_similarity_score_mappings[user_handle_2] = similarity_score
+        if user_handle != user_handle_2:
+            user2_feature_vec = feature_matrix[int(user_handle_2)-1][1:]
+            similarity_score = compute_similarity_score(user_feature_vec, user2_feature_vec)
+            top_n_user_handles.append({'user_handle': int(user_handle_2), 'similarity_score': similarity_score})
 
-    return top_n_user_handles
+    # sort the users by similarity scores from highest to lowest, and return the top n
+    return sorted(top_n_user_handles, key=lambda k: k['similarity_score'], reverse=True)[:n]
 
 
 @app.route('/', methods=['GET'])
 def home(user_handle, n=5):
-    top_n_users = get_top_n_similar_users(user_handle, n)
+    # top_n_users = get_top_n_similar_users(user_handle, n)
+    # print(top_n_users)
     return "<h1> Hello 123 </h1>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    top_n_users = get_top_n_similar_users(user_handle=1, n=5)
+    print(top_n_users)
--- populate_db.py
+++ populate_db.py
@@ -1,17 +1,23 @@
 from db_schemas import UserAssessmentScores, UserCourseViews, CourseTags, UserInterests
 from sqlalchemy import create_engine, MetaData
+import numpy as np
 
 engine = create_engine('sqlite:///pluralsight.db', echo=True)
 meta = MetaData()
 
+csv_file_paths = ['data/user_assessment_scores.csv', 'data/user_course_views.csv', 'data/user_course_tags.csv',
+                  'data/user_interests.csv']
 
-def populate_db(create_tables):
+
+def populate_db(create_tables=False, populate_tables=False):
     """
     Create 4 tables
     1. user_assessment_scores
     2. user_course_views
     3. course_tags
     4. user_interests
+
+    TODO: Populate tables from csv files
     """
     if create_tables:
         UserAssessmentScores.__table__.create(engine)
@@ -19,7 +25,15 @@ def populate_db(create_tables):
         CourseTags.__table__.create(engine)
         UserInterests.__table__.create(engine)
 
+    if populate_tables:
+        data = np.genfromtxt(csv_file_paths[0], delimiter=',', skip_header=1)
+        for j in data:
+            record = UserAssessmentScores()
+
+        data = np.genfromtxt(csv_file_paths[1], delimiter=',', skip_header=1)
+
 
 if __name__ == '__main__':
-    create_tbls = True
-    populate_db(create_tbls)
+    create_tbls = False
+    populate_tbls = True
+    populate_db(create_tbls, populate_tbls)
